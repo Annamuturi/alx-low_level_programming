461be044d36d40bb540ec4601cab4a19eb366d05
Anna <muturiwannah83@gmail>
2023-02-21 14:09:31 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - make alphatet x10 times 
+ * print_alphabet_x10 - make alphabet x10 times 
  * Return: void
  */
+
 void print_alphabet_x10(void)
+
 {
 	char c;
 	int i = 0;
