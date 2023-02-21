9d1327902e0538ea278e9c556cfcaa6526498c2f
Anna <muturiwannah83@gmail>
2023-02-21 10:56:47 -0800

    "_putch"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * *main - prints _purchar 
- * 
- * Return: Always 0. in success
+ * main - prints _putchar
+ *
+ * Return: Always 0 in success
  */
 int main(void)
 {
-   _putchar(95); /*95 is asci for _*/
-   _putchar('p');
-   _putchar('u');
-   _putchar('t');
-   _putchar('c');
-   _putchar('h');
-   _putchar('a');
-   _putchar('r');
-   _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-   return (0);
-
+	return (0);
 }
