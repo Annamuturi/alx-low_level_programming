97b06bb95b04d1f0bcb49a27b6a842cb8e3ce9b8
Anna <muturiwannah83@gmail>
2023-03-13 07:42:49 -0700

    "what you answer to"

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name, and new line
+ * @argc: Argument count/number.
+ * @argv: Argument vector , array of pointr to the argument.
+ *
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+
+	return(0);
+}
