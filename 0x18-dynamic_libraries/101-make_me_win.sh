7cc607a3a60a45c9ee7f800dd145fdb9dbee4d93
Annamuturi <muturiwannah83@gmail.com>
2023-05-26 14:08:40 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Annamuturi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -O /tmp/nrandom.so https://github.com/Annamuturi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so 
